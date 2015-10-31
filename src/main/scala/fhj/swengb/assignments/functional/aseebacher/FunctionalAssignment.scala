35893a6895254c982cce9d9167a904594384af95
andseeb <andreas.seebacher@outlook.at>
2015-10-31 13:32:48 +0100

    implemented new requirements for xs

--- src/main/scala/fhj/swengb/assignments/functional/aseebacher/FunctionalAssignment.scala
+++ src/main/scala/fhj/swengb/assignments/functional/aseebacher/FunctionalAssignment.scala
@@ -19,7 +19,7 @@ case class FunctionalAssignment(lowerBound: Int = 1,
     * xs(i) + factor == xs(i + 1) (for i > 0 and i <= 1000)
     *
     */
-  val xs: Seq[Int] = lowerBound to upperBound by factor
+  val xs: Seq[Int] = lowerBound to upperBound
 
   /**
     * provide the sum of all elements defined in xs
